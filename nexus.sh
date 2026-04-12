a66778ba41820a82fa146e2ea3cb3beca2784e0a
reezahussen-del <reezahussen@gmail.com>
2026-04-12 16:25:08 +0530

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,11 @@
+yum install java-17-amazon-corretto -y
+cd /opt
+wget https://download.sonatype.com/nexus/3/nexus-3.91.0-07-linux-x86_64.tar.gz
+tar -zxvf nexus-3.91.0-07-linux-x86_64.tar.gz
+useradd nexus
+chown -R nexus:nexus nexus-3.91.0-07 sonatype-work
+su - nexus
+cd /opt
+cd nexus-3.91.0-07
+cd /bin
+./nexus start
