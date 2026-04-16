cf305c923e5887b14e780a99f2f24898cec732da
reezahussen-del <reezahussen@gmail.com>
2026-04-16 21:50:44 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,4 +1,4 @@
-yum install java-17-amazon-corretto -y
+yum install java-21-amazon-corretto -y
 cd /opt
 wget https://download.sonatype.com/nexus/3/nexus-3.91.0-07-linux-x86_64.tar.gz
 tar -zxvf nexus-3.91.0-07-linux-x86_64.tar.gz
